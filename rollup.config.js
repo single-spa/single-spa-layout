6fdda4f340e049fe96e2519f700ba5873a8e2214
Joel Denning <joeldenning@gmail.com>
2020-05-18 13:14:35 -0600

    Removing JSX

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,19 @@
 import { terser } from "rollup-plugin-terser";
 
 export default [
-  ...createConfig("esm"),
-  ...createConfig("system"),
-  ...createConfig("umd"),
+  createConfig("esm"),
+  createConfig("system"),
+  createConfig("umd"),
 ];
 
 function createConfig(format) {
-  return [
-    {
-      input: "./src/single-spa-layout.js",
-      output: {
-        format,
-        file: `dist/${format}/single-spa-layout.min.js`,
-        name: format === "umd" ? "singleSpaLayout" : null,
-      },
-      plugins: [terser()],
+  return {
+    input: "./src/single-spa-layout.js",
+    output: {
+      format,
+      file: `dist/${format}/single-spa-layout.min.js`,
+      name: format === "umd" ? "singleSpaLayout" : null,
     },
-    {
-      input: "./src/jsx-runtime.js",
-      output: {
-        format,
-        file: `dist/${format}/jsx-runtime.js`,
-        name: format === "umd" ? "singleSpaJsxRuntime" : null,
-      },
-    },
-  ];
+    plugins: [terser()],
+  };
 }
