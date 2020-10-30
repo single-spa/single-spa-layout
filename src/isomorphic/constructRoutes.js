e69485fdefe85ad395e74139c6f1e07149fbd414
Joel Denning <joeldenning@gmail.com>
2020-10-30 10:37:49 -0600

    More dealing with ES/CJS interop with webpack. Resolves #90 (#91)

--- src/isomorphic/constructRoutes.js
+++ src/isomorphic/constructRoutes.js
@@ -16,9 +16,10 @@ import { find } from "../utils/find";
 // The weird strings here are to trick rollup into not optimizing the import to avoid the * syntax
 const defaultStr = "d" + "efault";
 const pathToActiveWhenStr = "pathTo" + "ActiveWhen";
-const pathToActiveWhen = singleSpa[defaultStr]
-  ? singleSpa[defaultStr][pathToActiveWhenStr]
-  : singleSpa[pathToActiveWhenStr];
+const pathToActiveWhen =
+  typeof singleSpa[defaultStr] !== "undefined"
+    ? singleSpa[defaultStr][pathToActiveWhenStr]
+    : singleSpa[pathToActiveWhenStr];
 
 export const MISSING_PROP = typeof Symbol !== "undefined" ? Symbol() : "@";
 
