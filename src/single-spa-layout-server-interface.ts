536ceff5250757b095ebfded2a7bdaf29a8ba460
Simon Feiden <simon.feiden@real-digital.de>
2024-11-26 12:56:29 +0100

    export types from /server entrypoint

---
+++ src/single-spa-layout-server-interface.ts
@@ -0,0 +1,10 @@
+export * from "./single-spa-layout-server";
+export {
+  HTMLTemplateOptions,
+  ServerLayout,
+} from "./server/constructServerLayout";
+export {
+  AppToRender,
+  ApplicationRenderResult,
+  RenderResult,
+} from "./server/sendLayoutHTTPResponse";
